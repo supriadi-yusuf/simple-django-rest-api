26ff67b2363c87663e26f3b16fd1e280fee5d5d1
supriadi <supriadi96@gmail.com>
2024-08-25 16:45:31 +0700

    add another tests

--- app/simpleapp/tests/test_simple_app.py
+++ app/simpleapp/tests/test_simple_app.py
@@ -15,7 +15,7 @@ class SimpleAppTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_calculate_success1(self):
+    def test_calculate_success(self):
         """calculate 1 + 1 = 2"""
         payload = {
             'x': 1,
@@ -29,3 +29,54 @@ class SimpleAppTest(TestCase):
 
         # compare data
         self.assertEqual(result.data, {'result':2})
+
+    def test_calculate_success2(self):
+        """"calculate 10 + 25 = 35"""
+        payload = {
+            'x': 10,
+            'y': 25,
+        }
+
+        result = self.client.post(SIMPLE_URL, payload)
+
+        # compare status
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+        # compare data
+        self.assertEqual(result.data, {'result':35})
+
+    def test_calculate_fail(self):
+        """calculate 5 + 'a' """
+        payload = {
+            'x': 5,
+            'y': 'a',
+        }
+
+        result = self.client.post(SIMPLE_URL, payload)
+
+        # compare status
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_calculate_fail2(self):
+        """calculate 'b' + 50 """
+        payload = {
+            'x': 'b',
+            'y': 50,
+        }
+
+        result = self.client.post(SIMPLE_URL, payload)
+
+        # compare status
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_calculate_fail3(self):
+        """calculate 'a' + 'b' """
+        payload = {
+            'x': 'a',
+            'y': 'b',
+        }
+
+        result = self.client.post(SIMPLE_URL, payload)
+
+        # compare status
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
