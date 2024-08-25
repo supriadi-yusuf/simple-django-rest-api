978051f896d8f4ca697a826e49dc1e6273e3f697
supriadi <supriadi96@gmail.com>
2024-08-25 16:32:17 +0700

    add view

--- app/simpleapp/views.py
+++ app/simpleapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import views, parsers, response, status
+from . import serializers
 
-# Create your views here.
+class SimpleAppView(views.APIView):
+    """simple app view"""
+
+    def post(self, request, format=None):
+        """ calculation """
+
+        calcSerializer = serializers.CalculateSerializer(data=request.data)
+
+        if not calcSerializer.is_valid():
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+        x = calcSerializer.data.get('x')
+        y = calcSerializer.data.get('y')
+
+        return response.Response({'result': x + y}, status=status.HTTP_200_OK)
+
